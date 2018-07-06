1f7eb730ab715d6fe8b2902fc7c3bc2e03999578
origami99 <origamista99@gmail.com>
2018-07-06 19:47:36 +0300

    Added "Subset of Sum S"

---
+++ Challenges/Arrays/Subset of Sum S/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = int.Parse(Console.ReadLine());
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        bool[,] solution = new bool[nums.Length + 1, sum + 1];
+
+        for (int i = 1; i <= sum; i++)
+        {
+            solution[0, i] = false;
+        }
+
+        for (int i = 0; i <= nums.Length; i++)
+        {
+            solution[i, 0] = true;
+        }
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            for (int j = 1; j <= sum; j++)
+            {
+                solution[i, j] = solution[i - 1, j];
+
+                if (solution[i, j] == false && j >= nums[i - 1])
+                {
+                    solution[i, j] = solution[i, j] || solution[i - 1, j - nums[i - 1]];
+                }
+            }
+        }
+
+        bool containsSum = solution[nums.Length, sum];
+
+        Console.WriteLine(containsSum ? "yes" : "no");
+    }
+}
