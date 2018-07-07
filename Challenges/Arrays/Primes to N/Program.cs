1f54a92f821e00b2bb26c300ff2b90a7ce635176
origami99 <origamista99@gmail.com>
2018-07-07 21:18:15 +0300

    Solved the super-easy task "Reverse Array"

---
+++ Challenges/Arrays/Primes to N/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<int> primes = new List<int>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (IsPrime(i))
+            {
+                primes.Add(i);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", primes));
+    }
+
+    private static bool IsPrime(int n)
+    {
+        for (int j = 2; j <= Math.Sqrt(n); j++)
+        {
+            if (n % j == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Challenges/Arrays/Reverse Array/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] array = Console.ReadLine().Split();
+
+        array = array.Reverse().ToArray();
+
+        Console.WriteLine(string.Join(", ", array));
+    }
+}
