b38137540532aa6b07bcc588bbb827c6612f97ab
origami99 <origamista99@gmail.com>
2018-07-07 19:49:04 +0300

    Solved the "Symmetric Arrays" task

---
+++ Challenges/Arrays/Symmetric Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(IsSymmetric(nums) ? "Yes" : "No");
+        }
+    }
+
+    private static bool IsSymmetric(int[] nums)
+    {
+        for (int i = 0; i < nums.Length / 2; i++)
+        {
+            int leftElem = nums[i];
+            int rightElem = nums[nums.Length - i - 1];
+
+            if (leftElem != rightElem)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
