f3845f4c6c7de5a7787c32b66968f66604048e66
origami99 <origamista99@gmail.com>
2018-07-08 21:33:26 +0300

    Solved new task/tasks

---
+++ Challenges/Conditions/Sort Three Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        if (a < b)
+            Swap(ref a, ref b);
+        if (b < c)
+            Swap(ref b, ref c);
+        if (a < b)
+            Swap(ref a, ref b);
+
+        Console.WriteLine($"{a} {b} {c}");
+    }
+
+    private static void Swap(ref int x, ref int y)
+    {
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+    }
+}
