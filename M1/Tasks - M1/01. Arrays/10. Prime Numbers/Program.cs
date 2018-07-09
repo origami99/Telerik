25c03dfe620542fa2d71fb5419a5fb9192f3dd61
origami99 <origamista99@gmail.com>
2018-07-10 00:44:59 +0300

    Solved the "Prime Numbers" task

---
+++ M1/Tasks - M1/01. Arrays/10. Prime Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        for (int i = num; i >= 1; i--)
+        {
+            if (IsPrimeFast(i))
+            {
+                Console.WriteLine(i);
+                return;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Fast method for finding is the number prime.
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    private static bool IsPrimeFast(int n)
+    {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i = i + 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
