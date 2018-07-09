2beb16206dad922a0f4fe782d8cdabf77dbd8302
origami99 <origamista99@gmail.com>
2018-07-09 22:51:28 +0300

    Solved "03. Compare char arrays"

--- M1/Tasks - M1/01. Arrays/03. Compare char arrays/Program.cs
+++ M1/Tasks - M1/01. Arrays/03. Compare char arrays/Program.cs
@@ -3,4 +3,31 @@ using System.Linq;
 
 class Program
 {
+    static void Main()
+    {
+        char[] charArr1 = Console.ReadLine().ToCharArray();
+        char[] charArr2 = Console.ReadLine().ToCharArray();
+
+        string equality = GetArrEquality(charArr1, charArr2);
+
+        Console.WriteLine(equality);
+    }
+
+    private static string GetArrEquality(char[] charArr1, char[] charArr2)
+    {
+        int minLen = new[] { charArr1.Length, charArr2.Length }.Min();
+
+        for (int i = 0; i < minLen; i++)
+        {
+            if (charArr1[i] != charArr2[i])
+            {
+                if (charArr1[i] > charArr2[i])
+                    return ">";
+                else
+                    return "<";
+            }
+        }
+
+        return "=";
+    }
 }
