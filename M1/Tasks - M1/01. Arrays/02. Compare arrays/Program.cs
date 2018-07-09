4924fbf142276afc1e6c805bec6206af2820098f
origami99 <origamista99@gmail.com>
2018-07-09 22:50:24 +0300

    Solved "02. Compare arrays"

--- M1/Tasks - M1/01. Arrays/02. Compare arrays/Program.cs
+++ M1/Tasks - M1/01. Arrays/02. Compare arrays/Program.cs
@@ -2,4 +2,34 @@
 
 class Program
 {
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] firstArr = new string[n];
+        string[] secondArr = new string[n];
+
+        FillArr(n, firstArr);
+        FillArr(n, secondArr);
+
+        bool areEqual = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (firstArr[i] != secondArr[i])
+            {
+                areEqual = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(areEqual ? "Equal" : "Not equal");
+    }
+
+    private static void FillArr(int n, string[] firstArr)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            firstArr[i] = Console.ReadLine();
+        }
+    }
 }
