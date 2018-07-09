35d9170007d7f2f38b3e7ac028285aa5a29f919b
origami99 <origamista99@gmail.com>
2018-07-09 22:59:11 +0300

    Solved "04. Maximal sequence"

---
+++ M1/Tasks - M1/01. Arrays/04. Maximal sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] arr = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine(GetMaxOccurs(arr));
+    }
+
+    private static int GetMaxOccurs(string[] arr)
+    {
+        return arr.Select((num, index) => new { Value = num, Index = index })
+            .OrderBy(x => x.Value)
+            .Select((n, i) => new { n.Value, Diff = i - n.Index })
+            .GroupBy(s => new { s.Value, s.Diff })
+            .Select(g => g.Count())
+            .Max();
+    }
+}
