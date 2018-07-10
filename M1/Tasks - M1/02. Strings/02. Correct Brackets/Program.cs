7a296f83b9285621a2b56492cd0a704310434534
origami99 <origamista99@gmail.com>
2018-07-10 18:29:32 +0300

    Solved "Correct Brackets"

---
+++ M1/Tasks - M1/02. Strings/02. Correct Brackets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string expression = Console.ReadLine();
+
+        int ballance = 0;
+        foreach (char symb in expression)
+        {
+            if (symb == '(')
+                ballance++;
+            if (symb == ')')
+                ballance--;
+
+            if (ballance < 0)
+            {
+                Console.WriteLine("Incorrect");
+                return;
+            }
+        }
+
+        Console.WriteLine(ballance == 0 ? "Correct" : "Incorrect");
+    }
+}
