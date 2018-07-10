f8e555c6b60bfe08668bc4a2ee1595fbfd10ae68
origami99 <origamista99@gmail.com>
2018-07-11 00:43:09 +0300

    Solved "06. Extract sentences"

---
+++ M1/Tasks - M1/02. Strings/06. Extract sentences/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string word = Console.ReadLine();
+        string text = Console.ReadLine();
+
+        string pattern = $@"[^.?!]*(?<=[.?\s!]){word}(?=[\s.?!])[^.?!]*[.?!]";
+        Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+        MatchCollection sentences = regex.Matches(text);
+
+        foreach (Match sentence in sentences)
+        {
+            Console.WriteLine(sentence.Value.TrimStart());
+        }
+    }
+}
