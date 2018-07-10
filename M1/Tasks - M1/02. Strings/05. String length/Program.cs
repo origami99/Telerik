e41dbc0ea4626853c42cba9a7b52a02a5066588a
origami99 <origamista99@gmail.com>
2018-07-10 20:56:45 +0300

    Solved "05. String length"

---
+++ M1/Tasks - M1/02. Strings/05. String length/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        string result = input + new string('*', 20 - input.Length);
+
+        Console.WriteLine(result);
+    }
+}
