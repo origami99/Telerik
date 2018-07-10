db4e46d9a0eccd394730cd5124642bc6a4b20593
origami99 <origamista99@gmail.com>
2018-07-10 15:30:36 +0300

    Added and solved both reverse strings

---
+++ M1/Tasks - M1/02. Strings/01. Reverse string/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            Console.Write(str[i]);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ M1/Tasks - M1/02. Strings/01. Reverse string_v2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+
+        str = new string(str.ToCharArray().Reverse().ToArray());
+
+        Console.WriteLine(str);
+    }
+}
