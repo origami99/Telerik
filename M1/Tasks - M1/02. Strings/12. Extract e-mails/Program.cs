a9cc49736471f2061251dfc52e6e16f60c7bd6d8
origami99 <origamista99@gmail.com>
2018-07-11 22:15:08 +0300

    Solved "12. Extract e-mails"

--- M1/Tasks - M1/02. Strings/12. Extract e-mails/Program.cs
+++ M1/Tasks - M1/02. Strings/12. Extract e-mails/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace _12._Extract_e_mails
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        string text = "yuki@abv.bg? Yes, this is just a string with random email like this steven@yahoo.com, or this marto@me.com. Ooo, by the way this one is not mine - origamista99@gmail.com... definitely";
+
+        Regex regex = new Regex(@"[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\.)+[a-z]+");
+
+        MatchCollection emails = regex.Matches(text);
+
+        foreach (Match email in emails)
+            Console.WriteLine(email.Value);
     }
 }
