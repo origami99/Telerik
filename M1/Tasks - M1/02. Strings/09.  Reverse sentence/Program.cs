1248dc7710ec97d6e8a2e6a3f738b8121a1ba533
origami99 <origamista99@gmail.com>
2018-07-11 17:55:26 +0300

    Solved "09. Reverse sentence"

---
+++ M1/Tasks - M1/02. Strings/09.  Reverse sentence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string sentence = Console.ReadLine();
+
+        // doesn't support multiple chars at the end, like: (...), (?!), etc.
+        char lastSymb = sentence.Last(); 
+        sentence = sentence.Remove(sentence.Length - 1);
+
+        string[] sentenceElements = Regex.Split(sentence, @"([\s\,\-]+)");
+        sentenceElements = sentenceElements.Reverse().ToArray();
+
+        string reversed = string.Join("", sentenceElements) + lastSymb;
+        Console.WriteLine(reversed);
+    }
+}
