acb987ad7f1f516224b049b9f6430104e84cbbba
origami99 <origamista99@gmail.com>
2018-07-11 18:39:26 +0300

    Solved "08. Date difference"

---
+++ M1/Tasks - M1/02. Strings/11. Date difference/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        // test if first date is newer
+        DateTime firstDate = DateTime.ParseExact(Console.ReadLine(), "d.MM.yyyy", CultureInfo.InvariantCulture);
+        DateTime secondDate = DateTime.ParseExact(Console.ReadLine(), "d.MM.yyyy", CultureInfo.InvariantCulture);
+
+        int days = Math.Abs((firstDate - secondDate).Days);
+        Console.WriteLine($"Distance: {days} days");
+    }
+}
