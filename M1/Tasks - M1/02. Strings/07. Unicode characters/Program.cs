e9b6529520e9cb15dc57f46431bde0220c517b40
origami99 <origamista99@gmail.com>
2018-07-11 10:02:18 +0300

    Solved 07. Unicode characters

---
+++ M1/Tasks - M1/02. Strings/07. Unicode characters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        foreach (var symb in text)
+        {
+            Console.Write($@"\u{(int)symb:x4}");
+        }
+        Console.WriteLine();
+    }
+}
