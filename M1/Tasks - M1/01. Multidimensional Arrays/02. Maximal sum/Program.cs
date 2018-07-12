0c0ce9ee3f1812f6655f51e2cc1ffa5590394131
origami99 <origamista99@gmail.com>
2018-07-12 13:46:55 +0300

    Solved "02. Maximal sum"

---
+++ M1/Tasks - M1/01. Multidimensional Arrays/02. Maximal sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] size = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int r = int.Parse(size[0]);
+        int c = int.Parse(size[1]);
+
+        int[][] matrix = new int[r][];
+        for (int row = 0; row < r; row++)
+        {
+            matrix[row] = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        }
+
+        int maxSum = int.MinValue;
+
+        for (int row = 0; row < r - 2; row++)
+        {
+            for (int col = 0; col < c - 2; col++)
+            {
+                int sum = GetSum(matrix, row, col);
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+        }
+
+        Console.WriteLine(maxSum);
+    }
+
+    public static int GetSum(int[][] matrix, int row, int col)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                sum += matrix[row + i][col + j];
+            }
+        }
+
+        return sum;
+    }
+}
