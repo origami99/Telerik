616fae44b05310d5f6a41a077b7f05e3c2048657
origami99 <origamista99@gmail.com>
2018-07-14 23:17:34 +0300

    Added "Square root"

---
+++ M1/Tasks - M1/03. Exception Handling/01. Square root/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            decimal num = decimal.Parse(Console.ReadLine());
+
+            decimal sqrt = (decimal)Math.Sqrt((double)num);
+
+            Console.WriteLine($"{sqrt:F3}");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
