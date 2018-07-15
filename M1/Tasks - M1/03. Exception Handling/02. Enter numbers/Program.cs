dfeeff7270701995e05c6c87c438742f55222847
origami99 <origamista99@gmail.com>
2018-07-15 13:08:22 +0300

    Solved "02. Enter numbers"

--- M1/Tasks - M1/03. Exception Handling/02. Enter numbers/Program.cs
+++ M1/Tasks - M1/03. Exception Handling/02. Enter numbers/Program.cs
@@ -4,11 +4,31 @@ class Program
 {
     static void Main()
     {
-        ReadNumber(start, end);
+        ReadNumber(1, 100);
     }
 
     private static void ReadNumber(int start, int end)
     {
-        
+        int prevNum = 1;
+        for (int i = 0; i < 10; i++)
+        {
+            // without try-catch block the behavior will be the same
+            int num;
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            if (num <= prevNum || num >= 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            prevNum = num;
+        }
     }
 }
