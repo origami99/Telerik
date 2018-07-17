2a188adcbe567359c3f040b842f8956fc213e980
origami99 <origamista99@gmail.com>
2018-07-17 23:34:38 +0300

    Added "GCD"

---
+++ Challenges/Numeral Systems/GCD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class GCD
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+
+        while (b != 0)
+        {
+            int previousB = b;
+            b = a % b;
+            a = previousB;
+        }
+
+        Console.WriteLine(a);
+    }
+}
