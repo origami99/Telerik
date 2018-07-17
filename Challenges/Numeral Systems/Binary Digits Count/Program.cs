222b7ebc6f1722973693f463f63017d323b9a8c3
origami99 <origamista99@gmail.com>
2018-07-18 00:00:56 +0300

    Added "Binary Digits Count"

---
+++ Challenges/Numeral Systems/Binary Digits Count/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        char b = char.Parse(Console.ReadLine());
+        long n = long.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            long num = long.Parse(Console.ReadLine());
+            string bin = Convert.ToString(num, 2);
+            Console.WriteLine(bin.Count(x => x == b));
+        }
+    }
+}
