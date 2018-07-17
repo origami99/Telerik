aa4abfaf3491d3a337f62258a23f7abb0d20bf71
origami99 <origamista99@gmail.com>
2018-07-17 23:37:38 +0300

    Added "N Factorial"

---
+++ Challenges/Numeral Systems/N Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+class BigFactorial
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(CalcFactorial(n));
+    }
+
+    private static BigInteger CalcFactorial(int n)
+    {
+        BigInteger factorial = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
