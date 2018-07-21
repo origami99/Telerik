f7f0233fe41146b96fba9624d3904c8fd26faba5
origami99 <origamista99@gmail.com>
2018-07-21 15:38:13 +0300

    Solved "Ranks"

---
+++ Challenges/Loops/Ranks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.ReadLine();
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] sortedArr = arr.OrderBy(x => -x).ToArray();
+        Console.WriteLine(string.Join(" ", arr.Select(x => x = Array.IndexOf(sortedArr, x) + 1)));
+    }
+}
