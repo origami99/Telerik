0429d1d7917544272ca2b9d7b6798bdd44a4bd57
origami99 <origamista99@gmail.com>
2018-07-21 17:37:56 +0300

    Solved "Numbers"

---
+++ Challenges/Conditions/Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        string num = string.Empty;
+        string message;
+
+        while ((message = Console.ReadLine()) != "end")
+        {
+            if (message.StartsWith("set"))
+                num = message.Split().Last();
+            else if (message.StartsWith("front-add"))
+                num = message.Split().Last() + num;
+            else if (message.StartsWith("back-add"))
+                num += message.Split().Last();
+            else if (message.StartsWith("front-remove") && num != string.Empty)
+                num = new string(num.Skip(1).ToArray());
+            else if (message.StartsWith("back-remove") && num != string.Empty)
+                num = new string(num.SkipLast(1).ToArray());
+            else if (message.StartsWith("reverse"))
+                num = new string(num.ToArray().Reverse().ToArray());
+            else if (message.StartsWith("print"))
+                Console.WriteLine(num);
+        }
+    }
+}
