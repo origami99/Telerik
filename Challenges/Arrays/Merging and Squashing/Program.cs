450c9c8f4f17dd0fe385d77057ea9bc413fec566
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-15 22:57:55 +0300

    Solved "Merging and Squashing"

---
+++ Challenges/Arrays/Merging and Squashing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<string> mergedNums = new List<string>();
+        List<string> squashedNums = new List<string>();
+
+        string num = Console.ReadLine();
+        while (--n > 0)
+        {
+            string prevNum = num;
+            num = Console.ReadLine();
+
+            mergedNums.Add(prevNum[1] + "" + num[0]);
+            squashedNums.Add(prevNum[0] + $"{(char.GetNumericValue(prevNum[1]) + char.GetNumericValue(num[0])).ToString().Last()}" + num[1]);
+        }
+
+        Console.WriteLine(string.Join(" ", mergedNums));
+        Console.WriteLine(string.Join(" ", squashedNums));
+    }
+}
