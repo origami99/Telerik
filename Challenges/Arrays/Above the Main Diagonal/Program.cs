173edbedda9a6ec4cc9606f68b52aa879f979b7b
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-15 22:51:59 +0300

    Solved "Above The Main Diagonal"

---
+++ Challenges/Arrays/Above the Main Diagonal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        long sum = 0;
+        for (int row = 0; row < n - 1; row++)
+        {
+            for (int col = row + 1; col < n; col++)
+            {
+                sum += (long)Math.Pow(2, row + col);
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
