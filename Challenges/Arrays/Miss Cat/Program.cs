959503bb33f6e328148e7bc66e6275ea62e80ffe
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-15 22:58:13 +0300

    Solved "Miss Cat"

---
+++ Challenges/Arrays/Miss Cat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] cats = new int[10];
+
+        for (int i = 0; i < n; i++)
+            cats[int.Parse(Console.ReadLine()) - 1]++;
+
+        int mostVotes = -1;
+        int winner = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            if (cats[i] > mostVotes)
+            {
+                mostVotes = cats[i];
+                winner = i;
+            }
+        }
+
+        Console.WriteLine(winner + 1);
+    }
+}
