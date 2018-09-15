44c13ec3ad9dabab16be799db69f9be71f94ee7f
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-15 22:55:34 +0300

    Solved "Adding Polynomials_v2"

---
+++ Challenges/Arrays/Adding Polynomials_v2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] firstPolynom = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] secondPolynom = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Console.WriteLine(string.Join(" ", firstPolynom.Select((x, y) => x = x + secondPolynom[y])));
+    }
+}
