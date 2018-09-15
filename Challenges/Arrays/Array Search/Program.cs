aa3b55c23cb57b56d3ea386abc4a31f99a99e00f
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-15 22:55:52 +0300

    Solved "Array Search"

---
+++ Challenges/Arrays/Array Search/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] nums = Console.ReadLine().Split(',');
+        HashSet<string> set = new HashSet<string>(nums);
+        List<string> result = new List<string>();
+
+        for (int i = 1; i <= nums.Length; i++)
+            if (!set.Contains(i.ToString()))
+                result.Add(i.ToString());
+
+        Console.WriteLine(string.Join(",", result));
+    }
+}
