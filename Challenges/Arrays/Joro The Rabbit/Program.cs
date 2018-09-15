2cb88c4cb6d256168679fd9bd7b70d9e5dceb136
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-15 22:56:32 +0300

    Solved "Joro The Rabbit"

---
+++ Challenges/Arrays/Joro The Rabbit/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+class JoroTheRabbit
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+        int result = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 1; j < nums.Length; j++)
+            {
+                int index = i;
+                int len = 1;
+                int next = default(int);
+
+                while (next != i)
+                {
+                    next = index + j;
+
+                    if (index + j >= nums.Length)
+                        next -= nums.Length;
+
+                    if (nums[index] >= nums[next])
+                        break;
+
+                    index = next;
+                    len++;
+                }
+
+                if (len > result)
+                    result = len;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
