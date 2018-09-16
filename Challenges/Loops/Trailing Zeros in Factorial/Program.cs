d0d34a6ec8141bf3899911c7daeed49f9b353ce5
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-16 23:03:24 +0300

    Solved "Trailing Zeros in Factorial"

---
+++ Challenges/Loops/Trailing Zeros in Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int zeroes = 0;
+        for (int i = 1; ; i++)
+        {
+            int addZeroes = n / (int)Math.Pow(5, i);
+            zeroes += addZeroes;
+
+            if (addZeroes == 0) break;
+        }
+
+        Console.WriteLine(zeroes);
+    }
+}
