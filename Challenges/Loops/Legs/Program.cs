2708118f833e4d53fc2998ee8f89ae9c7a182324
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-16 23:02:28 +0300

    Solved "Legs"

---
+++ Challenges/Loops/Legs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sevensBound = (int)Math.Ceiling(n / 7.0);
+        int fivesBound = (int)Math.Ceiling(n / 5.0);
+        int twosBound = (int)Math.Ceiling(n / 2.0);
+
+        int combinations = 0;
+        for (int i = 0; i <= sevensBound; i++)
+        {
+            for (int j = 0; j <= fivesBound; j++)
+            {
+                for (int k = 0; k <= twosBound; k++)
+                {
+                    if ((i * 7) + (j * 5) + (k * 2) == n)
+                    {
+                        combinations++;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(combinations);
+    }
+}
