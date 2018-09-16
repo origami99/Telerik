d62bc293ffd6b8601f2dc8f1054d8b66fc51d0ec
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-16 23:02:58 +0300

    Solved "Numbers Triangle"

---
+++ Challenges/Loops/Numbers Triangle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = -n + 1; i < n; i++)
+        {
+            Console.WriteLine(string.Join(" ",
+                Enumerable.Range(1, n - Math.Abs(i))));
+        }
+    }
+}
