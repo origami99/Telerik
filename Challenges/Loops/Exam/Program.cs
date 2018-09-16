0a2011b89fc13d25b0db16e624278e4ed83e41a6
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-16 23:02:18 +0300

    Solved "Exam"

---
+++ Challenges/Loops/Exam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ReadLine();
+
+        HashSet<int> studentsNoPens = new HashSet<int>(Console.ReadLine().Split().Select(int.Parse));
+        HashSet<int> studentsWithSparePen = new HashSet<int>(Console.ReadLine().Split().Select(int.Parse));
+
+        int count = 0;
+
+        foreach (int student in studentsNoPens)
+        {
+            if (studentsWithSparePen.Contains(student - 1))
+            {
+                studentsWithSparePen.Remove(student - 1);
+                count++;
+            }
+            else if (studentsWithSparePen.Contains(student + 1))
+            {
+                studentsWithSparePen.Remove(student + 1);
+                count++;
+            }
+        }
+
+        Console.WriteLine(studentsNoPens.Count - count);
+    }
+}
