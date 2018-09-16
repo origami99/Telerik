e0365859bf07cc2ad210133e30fa9e85e44e5814
Nikolay Lubomirov <origamista99@gmail.com>
2018-09-16 23:01:22 +0300

    Solved "Least Majority Multiple"

---
+++ Challenges/Conditions/Least Majority Multiple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> nums = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+            nums.Add(int.Parse(Console.ReadLine()));
+
+        for (int i = 1; ; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < nums.Count; j++)
+            {
+                if (i % nums[j] == 0)
+                    count++;
+
+                if (count >= 3)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+        }
+    }
+}
